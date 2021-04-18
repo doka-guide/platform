cb3eb074aecf62d658e40f706a5464bfb821245b
Igor Korovchenko <igsekor@gmail.com>
2021-04-18 16:41:24 +0300

    Исправляет проверку наличия симлинков и добавляет поведение по умолчанию

--- make-links.js
+++ make-links.js
@@ -8,36 +8,28 @@ const DEFAULT_PATH_TO_CONTENT = process.env.PATH_TO_CONTENT
 const CONTENT_REP_FOLDERS = process.env.CONTENT_REP_FOLDERS.split(', ')
 const SYMLINKS_DEST = CONTENT_REP_FOLDERS.map(folder => path.join('src', folder))
 
-console.log(`Проверяю, установлены ли символические ссылки к ${SYMLINKS_DEST.join(', ')}`)
 const existingSymlinks = SYMLINKS_DEST.filter((dest) => {
   try {
-    fs.accessSync(dest)
-    return true
+    return fs.readlinkSync(dest)
   } catch (e) {
     return false
   }
 })
 
+console.log(existingSymlinks, SYMLINKS_DEST);
+  
 if (existingSymlinks.length === SYMLINKS_DEST.length) {
   console.log('Символические ссылки уже установлены')
   process.exit(0)
 }
 
-existingSymlinks.forEach((dest) => {
-  console.log(`Удаляю старую ссылку ${dest}`)
-  fs.unlinkSync(dest)
-})
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-
-rl.question(`Укажите путь к репозиторию с контентом [нажми Enter, если это "${DEFAULT_PATH_TO_CONTENT}"]:`, (answer) => {
-  let contentPath = answer.trim() || DEFAULT_PATH_TO_CONTENT
-  if (!path.isAbsolute(contentPath)) {
-    contentPath = path.relative('src', contentPath)
-  }
+const createLinks = (contentPath) => {
+  console.log(`Проверяю, установлены ли символические ссылки к ${SYMLINKS_DEST.join(', ')}`)
+  
+  existingSymlinks.forEach((dest) => {
+    console.log(`Удаляю старую ссылку ${dest}`)
+    fs.unlinkSync(dest)
+  })
 
   console.log('Создаю симлинки:')
   SYMLINKS_DEST.forEach((dest, i) => {
@@ -47,6 +39,24 @@ rl.question(`Укажите путь к репозиторию с контент
   })
 
   console.log('✅ Готово')
-  rl.close()
-})
+}
 
+let contentPath = DEFAULT_PATH_TO_CONTENT
+if (process.argv[2] === '--default') {
+  console.log('Использую настройки из .env')
+  createLinks(contentPath)
+} else {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  rl.question(`Укажите путь к репозиторию с контентом [нажми Enter, если это '${DEFAULT_PATH_TO_CONTENT}']:`, (answer) => {
+    contentPath = answer.trim() || DEFAULT_PATH_TO_CONTENT
+    if (!path.isAbsolute(contentPath)) {
+      contentPath = path.relative('src', contentPath)
+    }
+    createLinks(contentPath)
+    rl.close()
+  })
+}
