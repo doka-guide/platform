c8b89411cc919360a8ab98e43e38f118bfbef2b0
Egor Levchenko <antytoto@gmail.com>
2021-04-20 12:20:48 +0300

    Добавляет глобальные контстанты

--- config/constants.js
+++ config/constants.js
@@ -3,5 +3,8 @@ require('dotenv').config({ path: '.env' })
 module.exports = {
   mainSections: process.env.SECTIONS.split(', '),
   contentRepFolders: process.env.CONTENT_REP_FOLDERS.split(', '),
-  defaultPathToContent: process.env.PATH_TO_CONTENT
+  defaultPathToContent: process.env.PATH_TO_CONTENT,
+  dokaCommunityLink: process.env.DOKA_COMMUNITY,
+  platformRepLink: process.env.PLATFORM_REP,
+  contentRepLink: process.env.CONTENT_REP
 }
