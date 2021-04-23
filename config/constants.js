870851e2e46337c7c06e9ffdde06abea5337959d
Egor Levchenko <antytoto@gmail.com>
2021-04-23 13:52:16 +0300

    Правит имена переменных

--- config/constants.js
+++ config/constants.js
@@ -2,7 +2,7 @@ require('dotenv').config({ path: '.env' })
 
 module.exports = {
   mainSections: process.env.SECTIONS.split(', '),
-  contentRepFolders: process.env.CONTENT_REP_GITHUB_URL_FOLDERS.split(', '),
+  contentRepFolders: process.env.CONTENT_REP_FOLDERS.split(', '),
   defaultPathToContent: process.env.PATH_TO_CONTENT,
   dokaOrgLink: process.env.DOKA_ORG,
   platformRepLink: process.env.PLATFORM_REP_GITHUB_URL,
