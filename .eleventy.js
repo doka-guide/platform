78823ac8e28d19e0bc12a0787e5815dda645aedc
Egor Levchenko <50330458+furtivite@users.noreply.github.com>
2021-07-13 15:26:48 +0300

    Правит скрипт для путей (#255)

--- .eleventy.js
+++ .eleventy.js
@@ -150,7 +150,7 @@ module.exports = function(config) {
         const mediaElements = practicesSection.querySelectorAll('img, iframe')
         for (const element of mediaElements) {
           const oldLink = element.getAttribute('src');
-          const newLink = oldLink.replace('../', '');
+          const newLink = oldLink.replace('../', './');
           element.setAttribute('src', newLink);
         }
       }
