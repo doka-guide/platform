407aa59815830ed7c9307cc733d4079c7a8a4271
monochromer <monochromer@mail.ru>
2021-08-26 21:25:48 +0500

    Фиксит подсветку для невалидных меток кода

--- src/transforms/code-transform.js
+++ src/transforms/code-transform.js
@@ -1,5 +1,6 @@
-// const Prism = require('prismjs')
-// const prismLoadLanguages = require('prismjs/components/')
+const os = require('os')
+const Prism = require('prismjs')
+const prismLoadLanguages = require('prismjs/components/')
 
 // расстановка классов и атрибутов для элементов кода внутри тела статьи,
 // подсветка синтаксиса,
@@ -9,23 +10,25 @@
  */
 module.exports = function(DOM) {
   DOM.document.querySelector('.article__content')
-    ?.querySelectorAll('pre > code[class*="language-"]')
+    ?.querySelectorAll('pre[data-lang] > code')
     ?.forEach(codeBlock => {
-      // const lang = codeBlock.getAttribute('class')
-      //   .split(' ')
-      //   .map(cls => cls.trim())
-      //   .find(cls => cls.includes('language-'))
-      //   ?.split('-')[1] || 'unknown'
+      let lang = codeBlock.parentNode.getAttribute('data-lang').trim()
 
-      // prismLoadLanguages([lang])
+      if (lang === 'js') {
+        lang = 'javascript'
+      }
 
-      // const lines = Prism.highlight(codeBlock.textContent, Prism.languages[lang], lang)
-      //   .split(os.EOL)
-      //   .filter((line, index, linesArray) => (index === 0 || index === linesArray.length -1) && line.trim() === '' ? false : true)
-      //   .map((line) => `
-      //     <span class="code-block__line"><span class="code-block__line-number"></span>
-      //   <span class="code-block__line-content">${line}</span></span>
-      //   `)
+      if (lang) {
+        prismLoadLanguages([lang])
+
+        const lines = Prism.highlight(codeBlock.textContent, Prism.languages[lang], lang)
+          .split(os.EOL)
+          .filter((line, index, linesArray) => (index === 0 || index === linesArray.length -1) && line.trim() === '' ? false : true)
+          .map((line) => `<span class="code-block__line"><span class="code-block__line-number"></span><span class="code-block__line-content">${line}</span></span>`)
+          .join(os.EOL)
+
+        codeBlock.innerHTML = lines
+      }
 
       codeBlock.parentNode.classList.add('code-block font-theme font-theme--code')
       codeBlock.parentNode.setAttribute('tabindex', 0)
