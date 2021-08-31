c3156ae6f41ccb0433a4fb2661fbae5e24081a13
monochromer <monochromer@mail.ru>
2021-08-31 12:22:22 +0500

    Вёрстка страниц из папки "pages"

---
+++ src/views/page.11tydata.js
@@ -0,0 +1,23 @@
+module.exports = {
+  layout: 'base.njk',
+
+  pagination: {
+    data: 'collections.pages',
+    size: 1,
+    alias: 'pageObject',
+  },
+
+  permalink: "{{ (pageObject.data.location or pageObject.fileSlug) | slug }}/index.html",
+
+  eleventyComputed: {
+    title: function(data) {
+      const { pageObject } = data
+      return pageObject.data.title
+    },
+
+    description: function(data) {
+      const { pageObject } = data
+      return pageObject.data.description
+    }
+  }
+}
