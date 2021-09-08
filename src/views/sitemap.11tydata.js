fff971666a451047bfc61bc1cb6584e3113e217b
monochromer <monochromer@users.noreply.github.com>
2021-09-08 17:40:58 +0500

    Вариант починки рендера `sitemap.xml` (#331)

---
+++ src/views/sitemap.11tydata.js
@@ -0,0 +1,47 @@
+const { baseUrl, mainSections } = require('../../config/constants')
+
+module.exports = {
+  permalink: '/sitemap.xml',
+
+  eleventyExcludeFromCollections: true,
+
+  eleventyComputed: {
+    pages: function(data) {
+      const { collections } = data
+
+      const collectionsItems = [
+        ...collections['pages'],
+        ...collections['docs'],
+      ].map(item => {
+        const pathName = item.filePathStem.replace('index', '')
+        const url = `${baseUrl}${pathName}`
+
+        const dateField = item.data.updatedAt || item.data.createdAt
+        const date = dateField ? new Date(dateField) : (item.data.page.date || new Date())
+
+        return {
+          url,
+          date
+        }
+      })
+
+      const standalonePages = [
+        // главная страница
+        {
+          url: `${baseUrl}/`,
+          date: new Date()
+        },
+        // страница с индексами категорий
+        ...mainSections.map(section => ({
+          url: `${baseUrl}/${section}/`,
+          date: new Date()
+        }))
+      ]
+
+      return [
+        ...standalonePages,
+        ...collectionsItems
+      ]
+    }
+  },
+}
