38f6840ce3d5c8e94be2cba8eeab873fec3d259d
monochromer <monochromer@mail.ru>
2021-09-16 22:19:21 +0500

    Правит канонические ссылки

--- src/views/views.11tydata.js
+++ src/views/views.11tydata.js
@@ -1,7 +1,20 @@
+const { baseUrl } = require('../../config/constants')
+
 module.exports = {
   eleventyComputed: {
     hasCategory: function(data) {
       return !!(data.categoryName)
+    },
+
+    baseUrl,
+
+    pageUrl: function(data) {
+      return data.page.url
+    },
+
+    fullPageUrl: function(data) {
+      return data.baseUrl + data.pageUrl
     }
-  }
+  },
+
 }
