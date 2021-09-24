86988481dd3bac3eb2dc886309718ebfb7a14993
Igor Korovchenko <igsekor@gmail.com>
2021-09-24 19:02:41 +0300

    Выносит в отдельный скрипт

---
+++ social-cards.sh
@@ -0,0 +1,22 @@
+node make-links.js --default
+npm run build
+npm install --global screenshoteer
+base_path="/home/runner/work/platform/platform/"
+files=( $(ls dist/*/*/index.og.html) )
+for in_file in ${files[@]}; do
+  tmp=""
+  tmp_file="${base_path}${in_file}.t"
+  touch $tmp_file
+  while IFS= read -r line; do
+    line="${line//\/styles/../../styles}"
+    tmp="${tmp}${line}\n"
+  done <<< $(cat $in_file)
+  echo $tmp > $tmp_file
+  mv $tmp_file $in_file
+  input_file="file://${base_path}${in_file}"
+  covers="${in_file//index.og.html/images/covers/}"
+  mkdir -p $covers
+  screenshoteer --w 1200 --h 630 --url $input_file --file "${covers}og.png"
+  screenshoteer --w 1024 --h 1024 --url $input_file --file "${covers}twitter.png"
+  rm $in_file
+done
