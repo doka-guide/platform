38ffcf2c3b8eba5bfc74091a562ffa37889a80df
monochromer <monochromer@mail.ru>
2021-09-30 15:34:42 +0500

    Удалаяет лишние documentTitle

--- src/views/404.11tydata.js
+++ src/views/404.11tydata.js
@@ -1,12 +1,5 @@
-const { titleFormatter } = require('../libs/title-formatter/title-formatter')
-
 module.exports = {
   title: 'Страница не найдена',
   layout: 'base.njk',
   permalink: '/404/index.html',
-  eleventyComputed: {
-    documentTitle: function(data) {
-      return titleFormatter([data.title, 'Дока'])
-    }
-  }
 }
