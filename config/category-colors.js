697a84498806456fb08f33be0dfc9178ab2f7c8d
monochromer <monochromer@mail.ru>
2021-10-05 16:56:35 +0500

    нормализует цвета

--- config/category-colors.js
+++ config/category-colors.js
@@ -1,22 +1,22 @@
 module.exports = {
   html: {
     light: 'hsl(25 100% 59%)',
-    dark: 'hsl(25deg 70% 24%)'
+    dark: 'hsl(25 70% 24%)'
   },
   css: {
     light: 'hsl(209 100% 59%)',
-    dark: 'hsl(209deg 70% 24%)'
+    dark: 'hsl(209 70% 24%)'
   },
   js: {
     light: 'hsl(49 100% 58%)',
-    dark: 'hsl(49deg 74% 23%)'
+    dark: 'hsl(49 74% 23%)'
   },
   tools: {
     light: 'hsl(122 78% 58%)',
-    dark: 'hsl(122deg 56% 23%)'
+    dark: 'hsl(122 56% 23%)'
   },
   default: {
-    light: '#fff',
-    dark: '#000'
+    light: 'hsl(0 0% 100%)',
+    dark: 'hsl(0 0% 0%)'
   }
 }
