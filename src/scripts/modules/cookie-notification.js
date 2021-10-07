71f5e72298b840c90bc56476e187a43c32e6a865
monochromer <monochromer@mail.ru>
2021-10-07 11:11:15 +0500

    Добавляет баннер с cookie
    Fix: #480

---
+++ src/scripts/modules/cookie-notification.js
@@ -0,0 +1,29 @@
+function init() {
+  const banner = document.querySelector('.cookie-notification')
+  const button = banner?.querySelector('button')
+
+  if (!banner && !button) {
+    return
+  }
+
+  const storageKey = 'cookie-notification'
+
+  try {
+    const isCookieAccepted = JSON.parse(localStorage.getItem(storageKey))
+
+    if (isCookieAccepted) {
+      return
+    }
+  } catch(error) {
+    console.error(error)
+  }
+
+  banner.hidden = false
+
+  button.addEventListener('click', () => {
+    banner.hidden = true
+    localStorage.setItem(storageKey, true)
+  }, { once: true })
+}
+
+init()
