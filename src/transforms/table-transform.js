4892ea1442fcb857543150b71a2fcad0c7cad494
monochromer <monochromer@users.noreply.github.com>
2021-10-12 13:12:24 +0500

    Добавляет для таблиц обёртки с прокруткой для избежания переполнения (#524)
    * Добавляет для таблиц обёртки с прокруткой для избежания переполнения
    
    * Правка для grid-layout

---
+++ src/transforms/table-transform.js
@@ -0,0 +1,16 @@
+// Оборачивает таблицы в обретки со прокруткой
+/**
+ * @param {Window} window
+ */
+ module.exports = function(window) {
+  window.document
+    .querySelector('.content')
+    ?.querySelectorAll('table')
+    ?.forEach(tableElement => {
+      const tableWrapper= window.document.createElement('div')
+      tableWrapper.classList.add('table-wrapper')
+      tableWrapper.setAttribute('tabindex', 0)
+      tableElement.replaceWith(tableWrapper)
+      tableWrapper.appendChild(tableElement)
+    })
+}
