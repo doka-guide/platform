b738b80fe51ebcf4386a0a1514e82c83675c9e5a
Igor Korovchenko <igsekor@gmail.com>
2021-10-15 13:43:56 +0300

    Вводит новые поля для карточек по умолчанию (#575)
    * Вводит новые поля для карточек по умолчанию
    
    * Меняет имена полей
    
    * Дополняет путь к картинкам
    
    * Переносит логику из шаблонов во views
    
    * Выносит условия в соответствующие data-файлы
    
    * Принимает правки @monochromer
    
    Co-authored-by: monochromer <monochromer@users.noreply.github.com>
    
    * Принимает правки @monochromer
    
    Co-authored-by: monochromer <monochromer@users.noreply.github.com>
    
    * Оптимизирует условия
    
    Co-authored-by: monochromer <monochromer@users.noreply.github.com>

--- src/views/doc.11tydata.js
+++ src/views/doc.11tydata.js
@@ -83,6 +83,24 @@ module.exports = {
       return doc.filePathStem.replace('/index', '')
     },
 
+    defaultOpenGraphPath: function(data) {
+      const { doc, docPath } = data
+      if (doc.data?.cover?.og) {
+        return baseUrl + docPath + '/' + doc.data.cover.og
+      } else {
+        return data.fullPageUrl + 'images/covers/og.png'
+      }
+    },
+
+    defaultTwitterPath: function(data) {
+      const { doc, docPath } = data
+      if (doc.data?.cover?.twitter) {
+        return baseUrl + docPath + '/' + doc.data.cover.twitter
+      } else {
+        return data.fullPageUrl + 'images/covers/twitter.png'
+      }
+    },
+
     category: function(data) {
       const { doc } = data
       return doc.filePathStem.split('/')[1]
