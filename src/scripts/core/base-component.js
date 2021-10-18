7cd30a49073f4aea438a85c6eb8ad9c762d35be5
monochromer <monochromer@mail.ru>
2021-10-18 16:27:15 +0500

    Рефакторит форму обратной связи
    Fix: #618

---
+++ src/scripts/core/base-component.js
@@ -0,0 +1,14 @@
+class BaseComponent extends EventTarget {
+  emit(eventType, detail) {
+    this.dispatchEvent(new CustomEvent(eventType, {
+      detail
+    }))
+  }
+}
+
+Object.assign(BaseComponent.prototype, {
+  on: EventTarget.prototype.addEventListener,
+  off: EventTarget.prototype.removeEventListener,
+})
+
+export default BaseComponent
