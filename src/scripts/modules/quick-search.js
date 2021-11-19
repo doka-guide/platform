92ca2ef7c9cefff52aa5e89218e36222694c34fc
monochromer <monochromer@users.noreply.github.com>
2021-11-19 14:11:47 +0500

    Добавляет поддержку клавиатуры в быстром поиске (#749)
    * Добавляет поддержку клавиатуры в быстром поиске
    
    * Меняет тип события на keydown, отменяет действие по умолчанию

--- src/scripts/modules/quick-search.js
+++ src/scripts/modules/quick-search.js
@@ -1,5 +1,13 @@
 import BaseComponent from '../core/base-component.js'
 
+function getBox(element) {
+  return element.getBoundingClientRect()
+}
+
+function clamp(min, value, max) {
+  return Math.max(min, Math.min(value, max))
+}
+
 class QuickSearch extends BaseComponent {
   constructor({ rootElement }) {
     super()
@@ -8,7 +16,12 @@ class QuickSearch extends BaseComponent {
       rootElement,
       input: rootElement.querySelector('.search__input'),
       suggestionContainer: rootElement.querySelector('.search__suggestion'),
+      suggestionContent: rootElement.querySelector('.search__suggestion-content'),
       suggestionList: rootElement.querySelector('.suggestion-list'),
+    }
+
+    this.state = {
+      highlightedIndex: -1
     };
 
     [
@@ -18,13 +31,18 @@ class QuickSearch extends BaseComponent {
       'closeSuggestion',
       'closeSuggestionOnKeyUp',
       'closeSuggestionOnOutSideClick',
-      'onSearch'
+      'onSearch',
+      'onCursorChange',
     ].forEach(method => this[method] = this[method].bind(this))
 
     document.addEventListener('click', this.closeSuggestionOnOutSideClick)
     this.refs.rootElement.addEventListener('keyup', this.closeSuggestionOnKeyUp)
     this.refs.rootElement.addEventListener('submit', event => {
       if (this.refs.input.value.trim() === '') {
+        event.preventDefault()
+      }
+
+      if (this.state.highlightedIndex >= 0) {
         event.preventDefault()
       }
     })
@@ -57,16 +75,22 @@ class QuickSearch extends BaseComponent {
       input.value = ''
       input.blur()
     }
+
+    this.state.highlightedIndex = -1
   }
 
   openSuggestion() {
     const { suggestionContainer } = this.refs
+
     suggestionContainer?.classList.remove('search__suggestion--hide')
+    document.addEventListener('keydown', this.onCursorChange)
   }
 
   closeSuggestion() {
     const { suggestionContainer } = this.refs
+
     suggestionContainer?.classList.add('search__suggestion--hide')
+    document.removeEventListener('keydown', this.onCursorChange)
   }
 
   get isSuggestionOpen() {
@@ -82,8 +106,8 @@ class QuickSearch extends BaseComponent {
   }
 
   closeSuggestionOnOutSideClick(event) {
-    const { suggestionContainer } = this.refs
-    if (!suggestionContainer.contains(event.target)) {
+    const { rootElement } = this.refs
+    if (!rootElement.contains(event.target)) {
       this.closeSuggestion()
     }
   }
@@ -92,9 +116,81 @@ class QuickSearch extends BaseComponent {
     this.emit('search', event.target.value)
   }
 
+  onCursorChange(event) {
+    switch (event.code) {
+      case 'ArrowDown': {
+        this.moveCursor(1)
+        event.preventDefault()
+        break
+      }
+
+      case 'ArrowUp': {
+        this.moveCursor(-1)
+        event.preventDefault()
+        break
+      }
+
+      case 'Enter': {
+        this.selectHighlightedElement()
+        break
+      }
+    }
+  }
+
+  selectHighlightedElement() {
+    const { suggestionList } = this.refs
+    const links = suggestionList.querySelectorAll('.suggestion-list__link')
+
+    if (links.length === 0) {
+      return
+    }
+
+    const index = this.state.highlightedIndex
+    if (index < 0) {
+      return
+    }
+
+    links[index].click()
+  }
+
+  moveCursor(direction) {
+    const { suggestionList, suggestionContent } = this.refs
+    const links = suggestionList.querySelectorAll('.suggestion-list__link')
+
+    if (links.length === 0) {
+      return
+    }
+
+    suggestionList
+      .querySelector('.suggestion-list__link--highlighted')
+      ?.classList.remove('suggestion-list__link--highlighted')
+
+    const oldIndex = this.state.highlightedIndex
+    const newIndex = clamp(0, oldIndex + direction, links.length - 1)
+    this.state.highlightedIndex = newIndex
+
+    links[newIndex].classList.add('suggestion-list__link--highlighted')
+
+    const contentBox = getBox(suggestionContent)
+    const elementBox = getBox(links[newIndex])
+
+    if (elementBox.top < contentBox.top) {
+      suggestionContent.scrollTop += (elementBox.top - contentBox.top)
+    }
+
+    if (elementBox.bottom > contentBox.bottom) {
+      suggestionContent.scrollTop += (elementBox.bottom - contentBox.bottom)
+    }
+
+    // links[newIndex].scrollIntoView(newIndex < oldIndex)
+    // links[newIndex].scrollIntoView()
+  }
+
   clearOutput() {
     const { suggestionList } = this.refs
     suggestionList.innerHTML = ''
+
+    this.state.highlightedIndex = -1
   }
 
   renderResults(hitObjectList) {
