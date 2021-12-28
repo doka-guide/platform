94f270d5f8a46ed5962ed5434016499484db18e6
Alena Batitskaya <solar.rust@gmail.com>
2021-12-28 13:21:27 +0300

    Вносит правки

--- src/scripts/modules/snow.js
+++ src/scripts/modules/snow.js
@@ -1,20 +1,20 @@
-let snowflakes = document.querySelectorAll(".snowflake")
-let snowBtn = document.querySelector(".snow-btn__control")
+let snowflakes = document.querySelectorAll('.snow__flake')
+let snowBtn = document.querySelectorAll('.snow-btn__control')
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min
 }
 
 snowflakes.forEach(s => {
-  s.style.fontSize = getRndInteger(0.7, 1.5) + "em"
-  s.style.animationDuration = getRndInteger(20, 30) + "s"
-  s.style.animationDelay = getRndInteger(-1, snowflakes.length / 2) + "s"
+  s.style.fontSize = getRndInteger(0.7, 1.5) + 'em'
+  s.style.animationDuration = getRndInteger(20, 30) + 's'
+  s.style.animationDelay = getRndInteger(-1, snowflakes.length / 2) + 's'
 })
 
-snowBtn.addEventListener("change", function() {
-  if (this.checked) {
-    snowflakes.forEach(s => s.style.animationName = "snowfall")
+snowBtn.forEach(btn => btn.addEventListener('change', function(event) {
+  if (event.target?.value === 'yes') {
+    snowflakes.forEach(s => s.style.animationName = 'snowfall')
   } else {
-    snowflakes.forEach(s => s.style.animationName = "none")
+    snowflakes.forEach(s => s.style.animationName = 'none')
   }
-})
+}) )
