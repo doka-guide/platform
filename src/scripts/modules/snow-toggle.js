389a6d9b7586f6d4791337b83b259b38fabe2bfd
Alena Batitskaya <solar.rust@gmail.com>
2021-12-29 13:32:52 +0300

    Сохраняет состояние в localStorage

--- src/scripts/modules/snow-toggle.js
+++ src/scripts/modules/snow-toggle.js
@@ -1,3 +1,4 @@
+const STOR_KEY = 'snow'
 const snow = document.querySelector('.snow')
 let snowflakes = document.querySelectorAll('.snow__flake')
 const snowToggle = document.querySelector('.snow-toggle')
@@ -10,12 +11,25 @@ function getRndFloat(min, max) {
   return (Math.random() * (max - min) + min).toFixed(1)
 }
 
-snowflakes.forEach(snowflake => {
-  snowflake.style.fontSize = getRndFloat(0.7, 1.5) + 'em'
-  snowflake.style.animationDuration = getRndInteger(20, 30) + 's'
-  snowflake.style.animationDelay = getRndInteger(-1, snowflakes.length / 2) + 's'
-})
+function snowflakesRandom() {
+  snowflakes.forEach(snowflake => {
+    snowflake.style.fontSize = getRndFloat(0.7, 1.5) + 'em'
+    snowflake.style.animationDuration = getRndInteger(20, 30) + 's'
+    snowflake.style.animationDelay = getRndInteger(-1, snowflakes.length / 2) + 's'
+  })
+}
 
 snowToggle.addEventListener('change', event => {
- snow.style.setProperty('--animationName',  event.target?.value === 'yes' ? 'snowfall' : 'none')
+  snow.style.setProperty('--animationName',  event.target?.value === 'yes' ? 'snowfall' : 'none')
+  localStorage.setItem(STOR_KEY, event.target?.value)
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  let curStore = localStorage.getItem(STOR_KEY)
+
+  if (curStore) {
+    snowToggle.querySelector(`.snow-toggle__control[value='${curStore}']`).checked = true
+  } else if (curStore === 'yes' || !curStore) {
+    snowflakesRandom()
+  }
 })
