30e3278b6d1275d0bda56326032d80fdcdcb502e
Igor Korovchenko <igsekor@gmail.com>
2022-07-02 14:44:52 +0300

    Меняет дизайн карточки

--- src/views/sc.11tydata.js
+++ src/views/sc.11tydata.js
@@ -1,13 +1,3 @@
-const { mainSections } = require('../../config/constants')
-const { titleFormatter } = require('../libs/title-formatter/title-formatter')
-
-function getPersons(personKey) {
-  return function (data) {
-    const { doc } = data
-    return doc.data[personKey]
-  }
-}
-
 module.exports = {
   pagination: {
     data: 'collections.docs',
@@ -18,22 +8,10 @@ module.exports = {
   permalink: '{{doc.filePathStem}}.sc.html',
 
   eleventyComputed: {
-    title: function (data) {
-      const { doc } = data
-      return doc.data.title
-    },
-
     cover: function (data) {
       const { doc } = data
       return doc.data.cover
     },
-
-    description: function (data) {
-      const { doc } = data
-      return doc.data.description
-    },
-
-    authors: getPersons('authors'),
 
     docPath: function (data) {
       const { doc } = data
@@ -48,25 +26,6 @@ module.exports = {
     categoryName: function (data) {
       const { category, collections } = data
       return collections.articleIndexes.find((section) => section.fileSlug === category)?.data.name
-    },
-
-    documentTitle: function (data) {
-      return titleFormatter([data.title, data.categoryName, 'Дока'])
-    },
-
-    articleTag: function (data) {
-      const { doc } = data
-      return doc.data.tags[0]
-    },
-
-    articleCategory: function (data) {
-      const { docPath } = data
-      const categoryKeys = Object.keys(mainSections)
-      for (const index in categoryKeys) {
-        if (docPath.includes(categoryKeys[index])) {
-          return mainSections[categoryKeys[index]]
-        }
-      }
     },
   },
 }
