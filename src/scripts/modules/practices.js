2676855212eb8f0d9c5aabe450ddcbf0d91f41c0
Igor Korovchenko <igsekor@gmail.com>
2022-08-09 12:36:25 +0300

    Добавляет прокрутку к блоку после сворачивания

--- src/scripts/modules/practices.js
+++ src/scripts/modules/practices.js
@@ -26,6 +26,8 @@ function togglePractice(event) {
   if (isPressed) {
     element.innerHTML = '+ Развернуть'
     toggleTabIndex(summaryContainer, '-1')
+    console.log(contentContainer.offsetTop)
+    window.scrollTo(0, contentContainer.offsetTop - 150)
   } else {
     element.innerHTML = '+ Свернуть'
     toggleTabIndex(summaryContainer, '0')
