2474094244900cf15223279f253a2dd96457b3ae
Igor Korovchenko <igsekor@gmail.com>
2022-09-08 15:16:30 +0300

    Задаёт иной порядок перехода

--- src/scripts/modules/practices.js
+++ src/scripts/modules/practices.js
@@ -4,15 +4,36 @@ function setTabIndex(list, newValue) {
   }
 }
 
-function toggleTabIndex(container, newValue) {
-  setTabIndex(container.getElementsByClassName('block-code__inner'), newValue)
-  setTabIndex(container.getElementsByClassName('block-code__copy-button'), newValue)
-  setTabIndex(container.getElementsByTagName('a'), newValue)
-  setTabIndex(container.getElementsByTagName('input'), newValue)
+function getActiveElements(container) {
+  const allElements = []
+  allElements.push(container.getElementsByClassName('block-code__inner'))
+  allElements.push(container.getElementsByClassName('block-code__copy-button'))
+  allElements.push(container.getElementsByTagName('a'))
+  allElements.push(container.getElementsByTagName('input'))
   for (let iframe of container.getElementsByTagName('iframe')) {
-    console.log(iframe.contentWindow.document)
-    setTabIndex(iframe.contentWindow.document.getElementsByTagName('a'), newValue)
-    setTabIndex(iframe.contentWindow.document.getElementsByTagName('input'), newValue)
+    allElements.push(iframe.contentWindow.document.getElementsByTagName('a'))
+    allElements.push(iframe.contentWindow.document.getElementsByTagName('input'))
+  }
+  return allElements
+}
+
+function toggleTabIndex(container, newValue) {
+  const activeElements = getActiveElements(container)
+  activeElements.forEach((elements) => {
+    setTabIndex(elements, newValue)
+  })
+}
+
+function tabKeyPressed(event) {
+  const summaryContainer = event.target.parentNode
+  const contentContainer = summaryContainer.parentNode
+  const activeElements = getActiveElements(contentContainer)
+  if (event.shiftKey && event.key === 'Tab') {
+    const collectionNumber = activeElements.length - 1
+    const elementNumber = activeElements[collectionNumber].length - 1
+    activeElements[collectionNumber][elementNumber].focus()
+  } else if (event.key === 'Tab') {
+    activeElements[0][0].focus()
   }
 }
 
@@ -25,10 +46,12 @@ function togglePractice(event) {
   contentContainer.classList.toggle('practices__content--open')
   if (isPressed) {
     element.innerHTML = '+ Развернуть'
+    element.removeEventListener('keydown', tabKeyPressed)
     toggleTabIndex(summaryContainer, '-1')
     window.scrollTo(0, contentContainer.offsetTop - 150)
   } else {
     element.innerHTML = '– Свернуть'
+    element.addEventListener('keydown', tabKeyPressed)
     toggleTabIndex(summaryContainer, '0')
   }
 }
