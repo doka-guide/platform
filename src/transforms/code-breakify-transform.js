df15b554cbffb2a340b53fcb6fddc01da637f6b5
Alexey Nikitchenko <106589280+Inventoris@users.noreply.github.com>
2022-10-03 22:10:38 +0300

    Добавляет дополнительные места для переноса текста (#983)

--- src/transforms/code-breakify-transform.js
+++ src/transforms/code-breakify-transform.js
@@ -1,6 +1,22 @@
 function breakify(content) {
   const symbols = ['.', ',', '-', '_', '=', ':', '~', '/', '\\', '?', '#', '%', '(', ')', '[', ']']
 
+  if (/[A-Z]/g.test(content)) {
+    switch (true) {
+      case /^[^A-Z]/.test(content):
+        content = content.replaceAll(/[A-Z]/g, (match) => `<wbr>${match}`)
+        break
+      case /[A-Z]{2,}/.test(content):
+        content = content.replaceAll(/[A-Z][a-z]+/g, (match) => `<wbr>${match}`)
+        break
+      case /[A-Z][a-z.]+[A-Z][a-z()]+/.test(content):
+        content = content.replaceAll(/[A-Z][a-z()]+$/g, (match) => `<wbr>${match}`)
+        break
+      default:
+        break
+    }
+  }
+
   for (const symbol of symbols) {
     content = content.replaceAll(symbol, (match) => `<wbr>${match}<wbr>`)
   }
