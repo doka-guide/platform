f8daeddde2687cd36434cead507aa61f85b60a26
Alena Batitskaia <batickaya.a@gmail.com>
2022-10-24 16:20:09 +0400

    Подставляет введённую почту

--- src/scripts/modules/subscribe-popup.js
+++ src/scripts/modules/subscribe-popup.js
@@ -1,9 +1,11 @@
 const subscribePopupForm = document.querySelector('.subscribe-popup__container')
 const subscribePopupSuccessText = document.querySelector('.subscribe-popup__success-text')
 const successClass = 'success'
+const userEmail = document.querySelector('.subscribe-popup__email')
 
 subscribePopupForm.addEventListener('submit', (event) => {
   event.preventDefault()
   subscribePopupForm.classList.toggle(successClass)
   subscribePopupSuccessText.classList.toggle(successClass)
+  userEmail.textContent = subscribePopupForm.querySelector('input').value
 })
