f1fa89f62b3f88300def7cec028d379e1b444354
Alena Batitskaia <batickaya.a@gmail.com>
2022-10-24 22:38:49 +0400

    Добавляет страницу подписки и настройки рассылки

---
+++ src/views/subscribe-settings.11tydata.js
@@ -0,0 +1,5 @@
+module.exports = {
+  title: 'Настройка рассылки от Доки',
+  layout: 'base.njk',
+  permalink: '/subscribe-settings/index.html',
+}
---
+++ src/views/subscribe.11tydata.js
@@ -0,0 +1,5 @@
+module.exports = {
+  title: 'Подписка на рассылку от Доки',
+  layout: 'base.njk',
+  permalink: '/subscribe/index.html',
+}
