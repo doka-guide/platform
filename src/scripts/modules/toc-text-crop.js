c1af4c312209b1d44eefeeaf9ac9368b70316b08
Alena Batitskaia <batickaya.a@gmail.com>
2022-10-28 15:36:25 +0400

    Выносит код в функцию, правит

--- src/scripts/modules/toc-text-crop.js
+++ src/scripts/modules/toc-text-crop.js
@@ -3,20 +3,24 @@
  */
 
 const tocLinks = Array.from(document.querySelectorAll('.toc__link'))
-const strLength = 90
+const strLength = 80
 
-tocLinks.forEach((link) => {
-  if (link.textContent.length > strLength) {
-    let stringArray = link.textContent.trim().replace(/\s+/g, ' ').split(' ')
+const clipContent = (linksArray, finalLength) => {
+  linksArray.forEach((link) => {
     let tmpString = ''
-    for (let i in stringArray) {
-      if (tmpString.length <= strLength) {
-        tmpString += stringArray[i] + ' '
-      } else {
-        return
+    if (link.textContent.length > finalLength) {
+      let stringArray = link.textContent.trim().replace(/\s+/g, ' ').split(' ')
+      for (let i in stringArray) {
+        if (tmpString.length <= finalLength) {
+          tmpString += stringArray[i] + ' '
+        } else {
+          break
+        }
       }
 
-      link.textContent = stringArray.slice(0, i).join(' ') + '…'
+      link.textContent = `${tmpString.trim()}…`
     }
-  }
-})
+  })
+}
+
+clipContent(tocLinks, strLength)
