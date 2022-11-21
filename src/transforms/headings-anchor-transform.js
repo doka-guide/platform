9688f09772e6e09151fea04523315a266c4a7bd8
Igor Korovchenko <igsekor@gmail.com>
2022-11-21 23:17:10 +0300

    Убирает угловые скобки для скрытого текста

--- src/transforms/headings-anchor-transform.js
+++ src/transforms/headings-anchor-transform.js
@@ -10,7 +10,7 @@ function createLinkMarkup(id, headingText) {
         <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
         <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
       </svg>
-      <span class="visually-hidden">Секция статьи "${headingText}"</span>
+      <span class="visually-hidden">Секция статьи "${headingText.replace(/(<|>)/g, '')}"</span>
     </a>
   `
 }
