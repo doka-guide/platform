64500d88e7e5cf952bc6320fa6580c8d316d204e
Алексей Никитченко <achestron@gmail.com>
2022-11-30 17:05:41 +0300

    Переносит jsdom, правит тест, успокаивает линтер

--- jest.setup.js
+++ jest.setup.js
@@ -2,3 +2,7 @@ const { TextEncoder, TextDecoder } = require('util')
 
 global.TextEncoder = TextEncoder
 global.TextDecoder = TextDecoder
+
+const { JSDOM } = require('jsdom')
+
+global.JSDOM = JSDOM
