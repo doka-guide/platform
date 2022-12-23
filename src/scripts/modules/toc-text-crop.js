c3e67e220068168659c243c9793cc716dd4199e1
Alexey Nikitchenko <106589280+Inventoris@users.noreply.github.com>
2022-12-23 17:30:06 +0300

    Рефакторит функцию `toc-text-crop.js` и добавляет тест (#1046)
    * Рефакторит функцию и добавляет тест
    
    * Предлагает альтернативный вариант

--- src/scripts/modules/toc-text-crop.js
+++ src/scripts/modules/toc-text-crop.js
@@ -2,25 +2,26 @@
  * Скрипт для обрезки длины заголовка секции в боковой навигации
  */
 
-const tocLinks = Array.from(document.querySelectorAll('.toc__link'))
-const strLength = 80
+const tocLinks = document.querySelectorAll('.toc__link')
+const MAX_LENGTH = 90
 
-const clipContent = (linksArray, finalLength) => {
+const clipContent = (linksArray, maxLength) => {
   linksArray.forEach((link) => {
-    let tmpString = ''
-    if (link.textContent.length > finalLength) {
-      let stringArray = link.textContent.trim().replace(/\s+/g, ' ').split(' ')
-      for (let i in stringArray) {
-        if (tmpString.length <= finalLength) {
-          tmpString += stringArray[i] + ' '
-        } else {
-          break
-        }
-      }
+    const linkText = link.textContent.trim().replace(/\s+/g, ' ')
 
-      link.textContent = `${tmpString.trim()}…`
+    if (linkText.length > maxLength) {
+      const linkTextCropped = linkText.substr(0, maxLength)
+      const indexOfLastSpace = linkTextCropped.lastIndexOf(' ')
+      const resultText = linkTextCropped.slice(0, indexOfLastSpace)
+
+      link.textContent = `${resultText}…`
     }
   })
 }
 
-clipContent(tocLinks, strLength)
+clipContent(tocLinks, MAX_LENGTH)
+
+module.exports = {
+  MAX_LENGTH,
+  clipContent,
+}
