0f32323ee7fdc586c455ab7e15f515eadef10fa0
TatianaFokina <fokinatatian@gmail.com>
2023-01-16 16:02:44 +0400

    Изменяет скрипт для дропдауна и кнопки

--- src/scripts/modules/filter-panel.js
+++ src/scripts/modules/filter-panel.js
@@ -1,6 +1,9 @@
 function init() {
   const filterPanel = document.querySelector('.filter-panel')
   const button = document.querySelector('.filter-panel__button')
+  const buttonName = document.querySelector('.float-button__name')
+  const buttonNameText = buttonName.innerHTML
+  let expanded = false
 
   if (!filterPanel && !button) {
     return
@@ -9,6 +12,16 @@ function init() {
   button.addEventListener('click', () => {
     filterPanel.classList.toggle('filter-panel--open')
   })
+
+  if (expanded) {
+    button.setAttribute('aria-expanded', 'false')
+    buttonName.innerHTML = buttonNameText
+    expanded = false
+  } else {
+    button.setAttribute('aria-expanded', 'true')
+    buttonName.innerHTML = 'Скрыть фильтр'
+    expanded = true
+  }
 }
 
 init()
