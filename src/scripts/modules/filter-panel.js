907c1fadd71135fa3bf5df6b401556526a45909f
Tatiana Fokina <fokinatatian@gmail.com>
2023-01-27 16:42:06 +0400

    Чинит скрипт для открытия меню с фильтрами (участники, поиск) (#1097)
    * Чинит скрипт для кнопки фильтров в поиске
    
    * Чинит скрипт для кнопки фильтров в участниках

--- src/scripts/modules/filter-panel.js
+++ src/scripts/modules/filter-panel.js
@@ -2,7 +2,6 @@ function init() {
   const filterPanel = document.querySelector('.filter-panel')
   const button = document.querySelector('.filter-panel__button')
   const buttonName = document.querySelector('.float-button__name')
-  const buttonNameText = buttonName.innerHTML
   let expanded = false
 
   if (!filterPanel && !button) {
@@ -11,17 +10,17 @@ function init() {
 
   button.addEventListener('click', () => {
     filterPanel.classList.toggle('filter-panel--open')
-  })
 
-  if (expanded) {
-    button.setAttribute('aria-expanded', 'false')
-    buttonName.innerHTML = buttonNameText
-    expanded = false
-  } else {
-    button.setAttribute('aria-expanded', 'true')
-    buttonName.innerHTML = 'Скрыть фильтр'
-    expanded = true
-  }
+    if (expanded) {
+      button.setAttribute('aria-expanded', 'false')
+      buttonName.innerHTML = 'Показать фильтр'
+      expanded = false
+    } else {
+      button.setAttribute('aria-expanded', 'true')
+      buttonName.innerHTML = 'Скрыть фильтр'
+      expanded = true
+    }
+  })
 }
 
 init()
