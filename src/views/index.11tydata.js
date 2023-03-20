a3d8b90761c5c6812277c9c2e641f6616fbea4fb
Igor Korovchenko <igsekor@gmail.com>
2023-03-20 21:37:47 +0300

    Устанавливает вёрстку и стили для главной страницы

--- src/views/index.11tydata.js
+++ src/views/index.11tydata.js
@@ -1,6 +1,9 @@
 module.exports = {
   layout: 'base.njk',
   permalink: '/',
-  documentTitle: 'Дока',
-  documentDescription: 'Документация для разработчиков на человеческом языке',
+  documentTitle: 'Гайд по трудоустройству — Спецпроект — Дока',
+  documentDescription:
+    'Спецпроект «Гайд по трудоустройству» команды Доки — документации для разработчиков на человеческом языке',
+  bodyClass: 'landing__body',
+  isMainPage: true,
 }
