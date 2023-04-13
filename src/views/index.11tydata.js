0d0511373f3f107cf0f2c5dab264f6f3949a3274
Igor Korovchenko <igsekor@gmail.com>
2023-04-13 16:06:38 +0300

    И ещё разок правит title

--- src/views/index.11tydata.js
+++ src/views/index.11tydata.js
@@ -1,7 +1,7 @@
 module.exports = {
   layout: 'base.njk',
   permalink: '/',
-  documentTitle: 'Гайд по трудоустройству — Дока',
+  documentTitle: 'Гайд по трудоустройству',
   documentDescription:
     'Спецпроект «Гайд по трудоустройству» команды Доки — документации для разработчиков на человеческом языке',
   bodyClass: 'landing__body',
@@ -9,7 +9,7 @@ module.exports = {
 
   eleventyComputed: {
     title: function () {
-      return 'Гайд по трудоустройству — Дока'
+      return 'Гайд по трудоустройству'
     },
 
     defaultOpenGraphPath: function (data) {
