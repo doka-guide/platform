0dbff6d38b4bcaef30fd00bb11375d16a0107440
Igor Korovchenko <igsekor@gmail.com>
2023-06-27 21:06:25 +0300

    Добавляет операции с базой данных

---
+++ src/scripts/modules/form-cache.js
@@ -0,0 +1,49 @@
+let db
+
+function getObjectStore(dbStoreName, mode) {
+  if (!db) {
+    return null
+  }
+  const tx = db.transaction(dbStoreName, mode)
+  return tx.objectStore(dbStoreName)
+}
+
+export function setupDb(dbStoreName, dbVersion, fields) {
+  const indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB
+  const request = indexedDB.open(dbStoreName, dbVersion)
+
+  request.onerror = (event) => {
+    console.error(event.target.errorCode)
+  }
+
+  request.onsuccess = (event) => {
+    db = event.target.result
+  }
+
+  request.onupgradeneeded = (event) => {
+    const thisDb = event.target.result
+
+    thisDb.onerror = (event) => {
+      console.error(event)
+    }
+
+    if (!thisDb.objectStoreNames.contains(dbStoreName)) {
+      const objectStore = thisDb.createObjectStore(dbStoreName, { keyPath: 'id', autoIncrement: true })
+
+      for (let i = 0; i < fields.length; i++) {
+        objectStore.createIndex(fields[i], fields[i], { unique: false })
+      }
+    }
+  }
+}
+
+export function saveTaskEvent(dbStoreName, newTask) {
+  const store = getObjectStore(dbStoreName, 'readwrite')
+  if (!store) {
+    return
+  }
+  const req = store.add(newTask)
+  req.onerror = () => {
+    console.error(this.error)
+  }
+}
