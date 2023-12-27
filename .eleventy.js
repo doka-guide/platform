8b975545c8e94cef1c56fd251575d61fe17f56c8
Igor Korovchenko <igsekor@gmail.com>
2023-12-27 21:50:08 +0300

    Добавляет копирование ресурсов для интервью

--- .eleventy.js
+++ .eleventy.js
@@ -387,7 +387,7 @@ module.exports = function (config) {
   config.addPassthroughCopy('src/robots.txt')
   config.addPassthroughCopy('src/fonts')
   config.addPassthroughCopy('src/images')
-  config.addPassthroughCopy('src/(css|html|js|tools|recipes|a11y|people)/**/!(*11tydata*)*.!(md)')
+  config.addPassthroughCopy('src/(css|html|js|tools|recipes|a11y|people|interviews)/**/!(*11tydata*)*.!(md)')
 
   return {
     dir: {
