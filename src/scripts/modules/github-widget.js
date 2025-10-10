ad346bd0f9e19ef8c9050f754407a0bd62c476fd
Alexey Nikitchenko <nikitchenkoad@gmail.com>
2025-10-10 14:32:43 +0300

    Добавляет проверку на 1000, вносит микро-правки

--- src/scripts/modules/github-widget.js
+++ src/scripts/modules/github-widget.js
@@ -7,9 +7,10 @@ async function init() {
     const response = await fetch('https://api.github.com/repos/doka-guide/content')
     const { stargazers_count: stargazersCount } = await response.json()
 
-    stargazers.textContent = `${(stargazersCount / 1000).toFixed(1).replace(/\.0$/, '')}к`
+    stargazers.textContent =
+      stargazersCount < 1000 ? stargazersCount : `${(stargazersCount / 1000).toFixed(1).replace(/\.0$/, '')}к`
   } catch (error) {
-    console.error('Ошибка формирования количества звёзд репозитория контента', error)
+    console.error('Не получилось сформировать число звёзд репозитория контента.', error)
   }
 }
 
